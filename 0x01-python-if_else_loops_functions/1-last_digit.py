47fc0d32c2b7329ee7d53eb6e60cc086f3c5ee40
SeckMohameth <mohamethseck34@gmail.com>
2019-04-30 18:39:51 +0000

    get last number and compare

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,9 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 if number < 0:
     lastNumber = number % -10
-
 else: lastNumber = number % 10
-
 if lastNumber > 5:
     print("Last digit of {} is {} and is\
  greater than 5".format(number, lastNumber))
