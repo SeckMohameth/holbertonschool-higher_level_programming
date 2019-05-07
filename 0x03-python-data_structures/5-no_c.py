4df85201438d5732a6fd6e37d62d096724dad93a
SeckMohameth <mohamethseck34@gmail.com>
2019-05-07 21:59:18 +0000

    hard but fun problem

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    my_string = my_string.replace("C", "")
-    my_string = my_string.replace("c", "")
-    return(my_string)
+    new_string = list(my_string)
+    for x in new_string:
+        if x is 'c' or x is 'C':
+            new_string.remove(x)
+    s = ""
+    new_string = s.join(new_string)
+    return(new_string)
