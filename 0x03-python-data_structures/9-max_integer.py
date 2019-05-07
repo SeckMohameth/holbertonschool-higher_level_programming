7ffd3a2ee561f8eca500ac75521472426c69e53e
SeckMohameth <mohamethseck34@gmail.com>
2019-05-07 23:19:47 +0000

    myMax

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list is None:
+    if not my_list:
         return None
     myMax = 0
     for x in my_list:
-        if myMax < x:
+        if myMax <= x:
             myMax = x
     return myMax
