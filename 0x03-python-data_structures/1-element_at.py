6e788894ea9cea11d560d49fff5effadb072016f
SeckMohameth <mohamethseck34@gmail.com>
2019-05-07 18:11:32 +0000

    chekcs

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if my_list == None and idx == None:
-        return None
-
-    if idx < 0:
-        return None
+    if idx >= 0:
+        return my_list[idx]
     if idx > len(my_list):
-        return None
-    for x in my_list:
         return my_list[idx]
