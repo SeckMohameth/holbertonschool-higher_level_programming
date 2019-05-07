50b10c29212c018c8031f4f690641752614a172a
SeckMohameth <mohamethseck34@gmail.com>
2019-05-07 16:11:03 +0000

    pushed at once

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    if idx > len(my_list):
+        return None
+    for x in my_list:
+        return my_list[idx]
