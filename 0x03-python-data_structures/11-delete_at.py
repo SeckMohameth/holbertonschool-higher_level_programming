fdca83cb762605decbc0f3f25d83468bd4b6a5cd
SeckMohameth <mohamethseck34@gmail.com>
2019-05-08 00:00:17 +0000

    remove at index

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
+    if not my_list:
+        return None
     new_list = my_list
     if idx < 0 or idx >= len(my_list):
         return new_list
