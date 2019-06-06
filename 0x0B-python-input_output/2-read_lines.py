f7f4ef53b68203258736caab9adf156c06074120
SeckMohameth <mohamethseck34@gmail.com>
2019-06-06 22:33:40 +0000

    fixed output errors by adding filename arg

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def read_lines(filename="", nb_lines=0):
     if nb_lines <= 0 or nb_lines >= len(filename):
-        with open("my_file_0.txt", encoding="utf-8") as myFile:
+        with open(filename, encoding="utf-8") as myFile:
             print(myFile.read(), end="")
     count = 0
-    with open("my_file_0.txt", encoding="utf-8") as myFile:
+    with open(filename, encoding="utf-8") as myFile:
         for lines in filename:
             while count < nb_lines:
                 print(myFile.readline(), end="")
