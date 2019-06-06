481c29cebacb52151d133a8518054aef3fddb4d9
SeckMohameth <mohamethseck34@gmail.com>
2019-06-06 22:31:42 +0000

    added filename argument

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def number_of_lines(filename=""):
-    with open("my_file_0.txt", encoding="utf-8") as myFile:
+    with open(filename, encoding="utf-8") as myFile:
         lines = 0
         for count in myFile:
             lines += 1
