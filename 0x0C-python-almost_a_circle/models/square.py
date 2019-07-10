d79a0a2f4d5f8aa09848656bcdf99b51cd307e12
SeckMohameth <mohamethseck34@gmail.com>
2019-07-10 21:51:32 +0000

    getting shit DONEgit add models/square.py git add models/square.py !

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -2,22 +2,25 @@
 
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     "Class rectangle"
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
+        self.size = size
 
-        def __str__(self):
-            "str function"
-            return("[Square] ({}) {}/{} - {}".format\
-                   (self.id, self.width, self.height, size))
+    def __str__(self):
+        "str function"
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.width, self.height, self.size)
+
     @property
-    def size(value):
+    def size(self):
         "Getter for size"
-        return(value)
+        return self.__width
 
     @size.setter
     def size(self, value):
         'Setter for size'
-        self.width = value
-        self.height = value
+        self.__width = value
+        self.__height = value
