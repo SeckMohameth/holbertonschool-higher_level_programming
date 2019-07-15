48d66efd6dcbac1eed5ea79f7971b8cbd75e8ef9
SeckMohameth <mohamethseck34@gmail.com>
2019-07-15 21:14:58 +0000

    deleting the lowest scores (score <= 5)

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,3 +1,3 @@
 -- Removes all records with a score <= 5
 -- SELECT score, name FROM second_table WHERE score <= 5
-DELETE from second_table where score <= 5;
+DELETE from second_table WHERE score <= 5;
