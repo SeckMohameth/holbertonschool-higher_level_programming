26b70183f769f87d3a980f9f6a152786195f8b50
SeckMohameth <mohamethseck34@gmail.com>
2019-07-15 23:44:34 +0000

    converting database and table to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,5 +2,5 @@
 ALTER DATABASE hbtn_0c_0 CHARACTER SET UTF8 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE second_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE second_table change name nameVARCHAR(256) CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_\
+ALTER TABLE second_table change name name VARCHAR(256) CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_\
  ci;
