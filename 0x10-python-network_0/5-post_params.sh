5c62df7cb60c7e63b1e5e51849f06f94bf112694
SeckMohameth <mohamethseck34@gmail.com>
2019-08-20 20:04:41 +0000

    made executable

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Sends POST request
-curl -s "email=hr@holbertonschool.com" "subject=I will always be here for PLD" "$1"
+curl -s -X "email=hr@holbertonschool.com" "subject=I will always be here for PLD" -d "$1"
