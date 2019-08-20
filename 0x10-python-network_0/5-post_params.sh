adfd6699672082fe79c2a9f4e3fe301cdb3f8528
SeckMohameth <mohamethseck34@gmail.com>
2019-08-20 20:12:12 +0000

    post

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Sends POST request
-curl -s -X "email=hr@holbertonschool.com" "subject=I will always be here for PLD" -d "$1"
+curl -s -d 'email=hr@holbertonschool.com' 'subject=I will always be here for PLD' "$1"
