9d3f392f79ba205c262641b6cd6bc89dc99cfa2a
SeckMohameth <mohamethseck34@gmail.com>
2019-08-26 21:22:21 +0000

    status code of url

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -4,6 +4,6 @@ import sys
 if __name__ == "__main__":
     url = requests.get(sys.argv[1])
     if url.status_code >= 400:
-        print("Error code: {}".format(status_code))
+        print("Error code: {}".format(url.status_code))
     else:
         print(url.text)
