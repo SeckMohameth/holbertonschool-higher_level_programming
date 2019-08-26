65796e747f987cffe9db90f22fcb88fe62d4a43a
SeckMohameth <mohamethseck34@gmail.com>
2019-08-26 21:20:51 +0000

    spelling error

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,7 +2,7 @@
 import requests
 import sys
 if __name__ == "__main__":
-    url = request.get(sys.argv[1])
+    url = requests.get(sys.argv[1])
     if url.status_code >= 400:
         print("Error code: {}".format(status_code))
     else:
