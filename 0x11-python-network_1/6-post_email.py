369e558b49aa8e5edf36916ae4500cfed5cba4d7
SeckMohameth <mohamethseck34@gmail.com>
2019-08-26 20:25:39 +0000

    fixed varible name

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,6 +4,6 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email_address = sys.argv[2]
-    mail = {'email': email}
+    mail = {'email': email_address}
     mine = requests.post(url, data=email_address)
-    print(mine.text)
+    print("{}".format(mine.text))
