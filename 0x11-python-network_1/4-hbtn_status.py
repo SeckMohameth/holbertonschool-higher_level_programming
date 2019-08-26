9f74ea6efd6633acc5bd43a4aa60b173492ad5d8
SeckMohameth <mohamethseck34@gmail.com>
2019-08-26 19:21:29 +0000

    spelling error

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
         html = requests.get('https://intranet.hbtn.io/status').text
         print("Body repsonse:")
         print("\t- type: {}".format(type(html)))
-        print("\t- tcontent: {}".format(html))
+        print("\t- content: {}".format(html))
