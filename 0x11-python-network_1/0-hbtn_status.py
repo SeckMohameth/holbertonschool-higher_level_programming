e043ed843888f0f0653c71fe4596d28f38539482
SeckMohameth <mohamethseck34@gmail.com>
2019-08-26 16:53:33 +0000

    should not execute when importing

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import urllib.request
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
-    print("- Body response:")
-    print("- type: {}".format(type(html)))
-    print("- content: {}".format(html))
-    print("- utf8 content: {}".format(html.decode('utf-8')))
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("- Body response:")
+        print("- type: {}".format(type(html)))
+        print("- content: {}".format(html))
+        print("- utf8 content: {}".format(html.decode('utf-8')))
