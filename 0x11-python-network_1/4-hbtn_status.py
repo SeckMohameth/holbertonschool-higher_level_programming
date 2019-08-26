0efbfb88d6edc6f3ac3e624eec6f01a2135fb90f
SeckMohameth <mohamethseck34@gmail.com>
2019-08-26 19:20:28 +0000

    Had to use a different request

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-import urllib.request
+import requests
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
+        html = requests.get('https://intranet.hbtn.io/status').text
         print("Body repsonse:")
         print("\t- type: {}".format(type(html)))
         print("\t- tcontent: {}".format(html))
