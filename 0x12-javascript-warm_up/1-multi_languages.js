97472302e61257f007cfc229a60be17b7def9b34
SeckMohameth <mohamethseck34@gmail.com>
2019-09-17 16:41:28 +0000

    fixed check

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-console.log("C is fun");
+let array = ["C is fun", "Python is cool", "Javascript is amazing"]
 
-console.log("Python is cool");
-
-console.log("Javascript is amazing");
+for (let x = 0; x < array.length; x++){
+    console.log(array[x]);
+}
