29958442852c6c246baa4c3b45ed8f9dc21bdeeb
SeckMohameth <mohamethseck34@gmail.com>
2019-09-17 21:18:46 +0000

    find second biggest number

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const args = process.argv;
+args.splice(0, 2);
+let second = 0;
+
+if (args.length <= 3) {
+  console.log('0');
+} else {
+  args.sort();
+  second = args[args.length - 2];
+  console.log(second);
+}
