ef4fd18435e9fe9a3b884111f347aadf196860f4
SeckMohameth <mohamethseck34@gmail.com>
2019-09-17 17:09:49 +0000

    fixed standard

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-let array = ["C is fun", "Python is cool", "Javascript is amazing"];
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
-for (let x = 0; x < array.length; x++){
-    console.log(array[x]);
+for (let x = 0; x < array.length; x++) {
+  console.log(array[x]);
 }
