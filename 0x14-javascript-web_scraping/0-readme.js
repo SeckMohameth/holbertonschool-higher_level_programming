69c1e5d3d1d0f56456192117f333ba9d9ccd8764
SeckMohameth <mohamethseck34@gmail.com>
2019-09-24 17:55:57 +0000

    missed check

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,8 +2,12 @@
 
 const args = process.argv.slice(2);
 
-const fs = require('fs');
-fs.readFile(args[0], 'utf8', (err, data) => {
-  if (err) console.log(err);
-  else console.log(data);
-});
+if (args.length !== 1) {
+    console.log('error');
+} else {
+    const fs = require('fs');
+    fs.readFile(args[0], 'utf8', (err, data) => {
+	if (err) console.log(err);
+	else console.log(data);
+    });
+}
